0d5a1b8c75bf7b3b8523c903dde3592c898cef0e
Valeria Aparecida <valeria777valeria@hotmail.com>
2026-01-30 14:23:05 -0300

    BEE 1001

---
+++ Estrutura Sequencial/Notas e Moedas/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int X = A + B;
+
+        System.out.println("X = " + X);
+
+
+        sc.close();
+    }
+}
