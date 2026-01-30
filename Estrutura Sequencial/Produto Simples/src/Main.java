fcd8eb624b3aa4cf7077099f69ffbd3cfcde0815
Valeria Aparecida <valeria777valeria@hotmail.com>
2026-01-30 16:14:30 -0300

    BEE 1004

---
+++ Estrutura Sequencial/Produto Simples/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int PROD = A * B;
+
+        System.out.println("PROD = " + PROD);
+
+    }
+}
