e6b47b29aa421f634efb73226f9037011a8d2111
Valeria Aparecida <valeria777valeria@hotmail.com>
2026-01-30 16:34:11 -0300

    BEE 1005

---
+++ Estrutura Sequencial/Media 1/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+
+        double MEDIA = ((A * 3.5) + (B * 7.5))/11;
+        System.out.printf("MEDIA = %.5f%n", MEDIA);
+    }
+}
