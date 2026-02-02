c1b1d35c2713fa70948809aa87ba528ccbaaac4e
Valeria Aparecida <valeria777valeria@hotmail.com>
2026-02-02 06:41:39 -0300

    BEE 1017

---
+++ Estrutura Sequencial/Gasto de Combustivel/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int tempoDeViajem = sc.nextInt();
+        int velocidadeMedia = sc.nextInt();
+
+        double litros = ((double)(tempoDeViajem * velocidadeMedia))/12.0;
+        System.out.printf("%.3f%n", litros);
+    }
+}
---
+++ Estrutura Sequencial/Media 2/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        double C = sc.nextDouble();
+
+        double MEDIA = ((A * 2.0) + (B * 3.0) + (C * 5.0))/10.0;
+        System.out.printf("MEDIA = %.1f%n", MEDIA);
+    }
+}
