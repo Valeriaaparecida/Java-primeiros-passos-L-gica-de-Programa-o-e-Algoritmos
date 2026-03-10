7998a879a2bcf659c8541e28d81bbc50229b5743
Valeria Aparecida <valeria777valeria@hotmail.com>
2026-03-10 07:05:02 -0300

    feat: resolvendo exercicío

---
+++ Estrutura Sequencial/Exercicios variados/BEE 1006/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+
+        int x = (A * B - C * D) ;
+        System.out.println("DIFERENCA = " + x);
+    }
+}
